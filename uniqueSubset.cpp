a2e8040f82fd1e6e94d54e8a5996f93fa105c718
aditya-din <adityash212@gmail.com>
2021-01-09 10:01:02 +0530

    Unique Subset Recusrion

--- uniqueSubset.cpp
+++ uniqueSubset.cpp
@@ -2,8 +2,30 @@
 #include<set>
 #include<vector>
 using namespace std;
+set <string> S;
+vector <string> V;
+void uniqueSubset(string input,string output){
+    if(input.size() == 0){
+        V.push_back(output);
+        return;
+    }
+    string output1 = output;
+    string output2 = output;
+    output2.push_back(input[0]);
+    input.erase(input.begin()+0);
+    // calling function 2 times
+    uniqueSubset(input,output1);
+    uniqueSubset(input,output2);
+}
 int main(){
-    set <int> S;
+    uniqueSubset("AAB","");
+    for(auto i=V.begin();i!= V.end() ; i++){
+        S.insert(*i);
+    }
+    for(auto i=S.begin();i!= S.end() ; i++){
+        cout << *i << endl;
+    }
+
 
 
 }
